6e67c1f63d0bf1ff578101844db89578b60c9c24
Efim Shikanov <darkcoderb@gmail.com>
2022-11-10 21:41:08 +0500

    feat: edited slice actions

--- src/app/slices/catsSlice.ts
+++ src/app/slices/catsSlice.ts
@@ -1,9 +1,11 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { Cat, CatsSliceState } from "../types";
 
-let favoriteCatsList = localStorage.favoriteCats
-  ? localStorage.favoriteCats
-  : [];
+if (!localStorage.favoriteCats) {
+  localStorage.setItem("favoriteCats", JSON.stringify([]));
+}
+
+let favoriteCatsList = JSON.parse(localStorage.favoriteCats);
 
 const initialState: CatsSliceState = {
   catsList: [],
@@ -15,14 +17,29 @@ export const catsSlice = createSlice({
   initialState,
   reducers: {
     setCats: (state, action: PayloadAction<Cat[]>) => {
-      console.log(action.payload);
+      state.catsList.push(...action.payload);
     },
-    addFavoriteCat: (state, action: PayloadAction<Cat>) => {
-
+    addToFavorite: (state, action: PayloadAction<string>) => {
+      let cat = state.catsList.find((cat) => cat.id === action.payload);
+      if (cat) {
+        cat.isLiked = true;
+        state.favoriteCatsList.push(cat);
+        localStorage.favoriteCats = JSON.stringify(state.favoriteCatsList);
+      }
+    },
+    removeFromFavorite: (state, action: PayloadAction<string>) => {
+      let cat = state.favoriteCatsList.find((cat) => cat.id === action.payload);
+      if (cat) {
+        const catIndex = state.favoriteCatsList.indexOf(cat);
+        state.favoriteCatsList.splice(catIndex, 1);
+        localStorage.favoriteCats = JSON.parse(
+          localStorage.favoriteCats
+        ).splice(catIndex, 1);
+      }
     },
   },
 });
 
-export const { setCats } = catsSlice.actions;
+export const { setCats, addToFavorite, removeFromFavorite } = catsSlice.actions;
 
 export default catsSlice.reducer;
